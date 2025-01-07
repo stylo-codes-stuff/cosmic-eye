1de9c9ee113e40251a62b9f3be9adbccf01c4590
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2025-01-06 18:09:08 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-
+function generateLocations(item_id){
+    switch (item_id){
+        case 1:
+            for (var i = 0;i<=5;i++){
+                console.log(random_choice(["cave","iron vein","cove"]))
+            }
+    }
+}
