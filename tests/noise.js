7a9e8ff1b8bd88f069c3ea605e2e567e6ebaf75c
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2025-03-21 07:00:25 +0000

    debugging and testing parameters for noisemap generation function

--- tests/noise.js
+++ tests/noise.js
@@ -1,3 +1,6 @@
+const seed = 55;
+const gen = lcg(seed);
+
 //js class for generating perlin noise gradient maps adapted from https://rtouti.github.io/graphics/perlin-noise-algorithm
 var frequency = .212
 class Vector2 {
@@ -7,15 +10,15 @@ class Vector2 {
 	}
 
 	dot(other) {
-		return this.x*other.x + this.y*other.y;
+		return this.x * other.x + this.y * other.y;
 	}
 }
 
 function Shuffle(arrayToShuffle) {
-	for(let e = arrayToShuffle.length-1; e > 0; e--) {
-		const index = Math.round(Math.random()*(e-1));
+	for (let e = arrayToShuffle.length - 1; e > 0; e--) {
+		const index = Math.round(gen() * (e - 1));
 		const temp = arrayToShuffle[e];
-		
+
 		arrayToShuffle[e] = arrayToShuffle[index];
 		arrayToShuffle[index] = temp;
 	}
@@ -23,16 +26,16 @@ function Shuffle(arrayToShuffle) {
 
 function MakePermutation() {
 	const permutation = [];
-	for(let i = 0; i < 256; i++) {
+	for (let i = 0; i < 256; i++) {
 		permutation.push(i);
 	}
 
 	Shuffle(permutation);
-	
-	for(let i = 0; i < 256; i++) {
+
+	for (let i = 0; i < 256; i++) {
 		permutation.push(permutation[i]);
 	}
-	
+
 	return permutation;
 }
 const Permutation = MakePermutation();
@@ -40,72 +43,97 @@ const Permutation = MakePermutation();
 function GetConstantVector(v) {
 	// v is the value from the permutation table
 	const h = v & 3;
-	if(h == 0)
+	if (h == 0)
 		return new Vector2(1.0, 1.0);
-	else if(h == 1)
+	else if (h == 1)
 		return new Vector2(-1.0, 1.0);
-	else if(h == 2)
+	else if (h == 2)
 		return new Vector2(-1.0, -1.0);
 	else
 		return new Vector2(1.0, -1.0);
 }
 
 function Fade(t) {
-	return ((6*t - 15)*t + 10)*t*t*t;
+	return ((6 * t - 15) * t + 10) * t * t * t;
 }
 
 function Lerp(t, a1, a2) {
-	return a1 + t*(a2-a1);
+	return a1 + t * (a2 - a1);
 }
 
 function Noise2D(x, y) {
 	const X = Math.floor(x) & 255;
 	const Y = Math.floor(y) & 255;
 
-	const xf = x-Math.floor(x);
-	const yf = y-Math.floor(y);
+	const xf = x - Math.floor(x);
+	const yf = y - Math.floor(y);
 
-	const topRight = new Vector2(xf-1.0, yf-1.0);
-	const topLeft = new Vector2(xf, yf-1.0);
-	const bottomRight = new Vector2(xf-1.0, yf);
+	const topRight = new Vector2(xf - 1.0, yf - 1.0);
+	const topLeft = new Vector2(xf, yf - 1.0);
+	const bottomRight = new Vector2(xf - 1.0, yf);
 	const bottomLeft = new Vector2(xf, yf);
-	
+
 	// Select a value from the permutation array for each of the 4 corners
-	const valueTopRight = Permutation[Permutation[X+1]+Y+1];
-	const valueTopLeft = Permutation[Permutation[X]+Y+1];
-	const valueBottomRight = Permutation[Permutation[X+1]+Y];
-	const valueBottomLeft = Permutation[Permutation[X]+Y];
-	
+	const valueTopRight = Permutation[Permutation[X + 1] + Y + 1];
+	const valueTopLeft = Permutation[Permutation[X] + Y + 1];
+	const valueBottomRight = Permutation[Permutation[X + 1] + Y];
+	const valueBottomLeft = Permutation[Permutation[X] + Y];
+
 	const dotTopRight = topRight.dot(GetConstantVector(valueTopRight));
 	const dotTopLeft = topLeft.dot(GetConstantVector(valueTopLeft));
 	const dotBottomRight = bottomRight.dot(GetConstantVector(valueBottomRight));
 	const dotBottomLeft = bottomLeft.dot(GetConstantVector(valueBottomLeft));
-	
+
 	const u = Fade(xf);
 	const v = Fade(yf);
-	
+
 	return Lerp(u,
 		Lerp(v, dotBottomLeft, dotTopLeft),
 		Lerp(v, dotBottomRight, dotTopRight)
 	);
 }
-var canvas = document.getElementById("noise_canvas")
-const ctx = canvas.getContext("2d");
-for(var x = 0;x<100;x++){
-  for(var y=0;y<100;y++){
-  var noise_val = Noise2D(x+frequency,y+frequency)
-    var rounded_val = Math.round(noise_val * 100) / 100;
-    if(rounded_val <0){
-      rounded_val *= -1;
-    }
-y    //similar to pen down in scratch
-      ctx.beginPath();
-      //creates a circle with the specified radius at said coordinates
-      ctx.fillRect(x*5,y*5,5,5)
-      //sets and fills it with the specified color
-      ctx.fillStyle = `rgb(0 0 0 / ${rounded_val})`;
-      ctx.fill();
-      //similar to pen down
-      ctx.closePath();
-  }
+function FractalBrownianMotion(x, y, numOctaves,amplitude, frequency, freq_mult, amp_mult) {
+	let result = 0.0;
+
+	for (let octave = 0; octave < numOctaves; octave++) {
+		const n = amplitude * Noise2D(x * frequency, y * frequency);
+		result += n;
+
+		amplitude *= amp_mult;
+		frequency *= freq_mult;
+	}
+
+	return result;
 }
+
+//generates a noisemap and then populates it with appropiate terrain
+
+function generateNoisemap(canvas_id,height, width, frequency, octaves,freq_mult,amp_mult) {
+	//id of the canvas that the noise map will be rendered onto
+	var canvas = document.getElementById(canvas_id)
+	var ctx = canvas.getContext("2d");
+	var noise_coords = [];
+	for (var x = 0; x < width; x++) {
+		for (var y = 0; y < height; y++) {
+			var noise_val = FractalBrownianMotion(x, y, octaves, 1, frequency,freq_mult,amp_mult)
+			var rounded_val = Math.round(noise_val * 100) / 100;
+			console.log(rounded_val)
+			noise_coords.push({x:x,y:y,value:rounded_val})
+
+		}
+		for(var coord = 0;coord<noise_coords.length-1;coord++){
+						//similar to pen down in scratch
+						ctx.beginPath();
+						//creates a circle with the specified radius at said coordinates
+						ctx.fillRect(noise_coords[coord].x * 10, noise_coords[coord].y * 10, 10, 10)
+						//sets and fills it with the specified color
+						ctx.fillStyle = `rgb(0 0 0 / ${noise_coords[coord].value})`;
+			
+						ctx.fill();
+						//similar to pen down
+						ctx.closePath();
+		}
+	}
+
+}
+generateNoisemap("noise_canvas",30,30,.2,2,.5,2)
